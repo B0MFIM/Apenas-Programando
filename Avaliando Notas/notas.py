c9d0bb43d84b61bb604c6a00426dd02514c1061d
Pedro Bomfim <85983211+B0MFIM@users.noreply.github.com>
2022-06-30 11:25:33 -0400

    Avaliando Notas v3
    Programa que lê notas de avaliações de uma pessoa, calcula a sua média e exibe sua situação.

--- Avaliando Notas/notas.py
+++ Avaliando Notas/notas.py
@@ -1,21 +1,37 @@
 # Definições
 def main():
+
+    # Definindo uma lista para as notas
+    notas = list()
+
     # Avaliação Normal
-    for c in range(1, 2+1):
-        nota = float(input(f"Digite a nota da {c}º avaliação: "))
-        notas.append(nota)
+    while True:
+        try:
+            for c in range(1, 2+1):
+                nota = float(input(f"Digite a nota da {c}º avaliação: "))
+                notas.append(nota)
+        except:
+            print("\033[0;31m< ERRO > valor inserido inválido < ERRO >\033[m")
+            notas.clear()
+            continue
+        else:
+            break
 
     # Avaliação Optativa
-    fezOpt = str(input(f"O Aluno fez a Optativa? [S/N] ")).upper().strip()
-    if fezOpt == "S":
-        nota = float(input(f"Digite a nota da Optativa: "))
-    elif fezOpt == "N":
-        nota = -1
-    else:
-        print("Valor Inválido")
+    while True:
+        fezOpt = str(input(f"O Aluno fez a Optativa? [S/N] ")).upper().strip()
+        if fezOpt == "S":
+            nota = float(input(f"Digite a nota da Optativa: "))
+            break
+        elif fezOpt == "N":
+            nota = -1
+            break
+        else:
+            print("\033[0;31m< ERRO > valor inserido inválido < ERRO >\033[m")
+            continue
 
     # Calcular Média do Semestre
-    if notas[0] < nota and notas[0] < notas[1]:
+    if notas[0] < nota and notas[0] <= notas[1]:
         media = (nota + notas[1]) / 2
     elif notas[1] < nota and notas[1] < notas[0]:
         media = (notas[0] + nota) / 2
@@ -24,13 +40,12 @@ def main():
         
     # Exibir Situação
     if media >= 6:
-        print("O Aluno foi APROVADO!!")
+        print("O Aluno foi \033[0;35mAPROVADO!!\033[m")
     elif media <= 3:
-        print("O Aluno foi REPROVADO!!")
+        print("O Aluno foi \033[0;31mREPROVADO!!\033[m")
     elif 3 < media < 6:
-        print("O Aluno está de EXAME FINAL!!")
+        print("O Aluno está de \033[0;33mRECUPERAÇÃO!!\033[m")
 
 
 # Principal
-notas = list()
 main()
