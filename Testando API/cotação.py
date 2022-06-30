20bead5d8166e4cf78bb7bb784856255925c6f47
Pedro Bomfim <85983211+B0MFIM@users.noreply.github.com>
2022-06-30 17:00:13 -0400

    API cotações

--- Testando API/cotação.py
+++ Testando API/cotação.py
@@ -2,4 +2,7 @@ import requests
 
 cotações = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
 cotações = cotações.json()
-print(cotações)
+cotação_dolar = cotações["USDBRL"]["bid"]
+cotação_euro = cotações["EURBRL"]["bid"]
+print(f"A cotação atual do Dolar é de: {cotação_dolar}")
+print(f"A cotação atual do EURO é de: {cotação_euro}")
