aaa7da39fa80c935f99d31610c0184fdd1bf6c4e
Pedro Bomfim <85983211+B0MFIM@users.noreply.github.com>
2022-07-07 11:36:52 -0400

    list comprehension

--- ConsultasCode/List Comprehension/main.py
+++ ConsultasCode/List Comprehension/main.py
@@ -6,6 +6,18 @@ for preço in lista_preços:
     nova_lista_preços.append(preço*2)
 print(nova_lista_preços)
 
-# caso 2, com list comprehesion
+# caso 1, com list comprehesion
 nova_lista_preços_2 = [preço * 2 for preço in lista_preços]
 print(nova_lista_preços_2)
+
+
+# caso 2, normal
+imposto = []
+for preço in lista_preços:
+    if preço > 1000:
+        imposto.append(preço * 0.5)
+print(imposto)
+
+# caso 2, com list comprehesion
+imposto_2 = [preço * 0.5 for preço in lista_preços if preço > 1000]
+print(imposto_2)
